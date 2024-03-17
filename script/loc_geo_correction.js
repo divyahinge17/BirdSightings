514f1f9d943f972d41c25cd9bfa79f5c7897a9aa
comkar893 <comkar893@gmail.com>
2024-03-17 18:16:49 -0400

    Revert "script: formating geo points and test queries"
    This reverts commit 5184eeda8e5e21da82a1e10ad349214325cfad0f.

--- script/loc_geo_correction.js
+++ script/loc_geo_correction.js
@@ -1,32 +1,5 @@
 const connection = new Mongo(`localhost:27017`);
 db = connection.getDB(`flock`);
 sightings1 = db.getCollection(`sightings`);
+
 sightings1.aggregate([{$out: "sightings2"}]);
-sightings2 = db.getCollection(`sightings2`);
-sightings2.updateMany(
-    {},
-    [
-        {
-            $set: {
-                location: {
-                    $cond: {
-                        if: { $or: [
-                            { $eq: ["$LONGITUDE",""]},
-                            { $eq: ["$LATITUDE",""]},
-                            { $eq: ["$LONGITUDE"," "]},
-                            { $eq: ["$LATITUDE"," "]}
-                        ]},
-                        then: null,
-                        else: {
-                            type: "Point",
-                            coordinates: ["$LONGITUDE", "$LATITUDE"]
-                        }
-                    }
-                }
-            }
-        },
-        {
-            $unset: ["LONGITUDE","LATITUDE"]
-        }
-    ]
-)
